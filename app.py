d80c8877fe7d4a3f972b71881b8941409f04baf8
Judy <judy.programer@gmail.com>
2015-12-28 01:25:08 +0800

    add _health API; don't load images.

--- app.py
+++ app.py
@@ -1,30 +1,56 @@
+from faker import Faker
 from flask import Flask, request, Response
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-# driver = webdriver.PhantomJS()
-driver = webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs',
-                             service_args=['--ignore-ssl-errors=true'])
+driver = webdriver.PhantomJS(
+    service_args='--disk-cache true --load-images false'.split()
+)
+# driver = webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs',
+#                              service_args=['--ignore-ssl-errors=true'])
+faker = Faker()
 
 
 @app.route("/_health", methods=['GET'])
 def check_health():
-    return 'I am ok.'
+    return 'I am OK!'
 
 
 @app.route("/", methods=['POST'])
 def fetch():
+    """ Fetching a page via PhantomJS.
+
+    :Args:
+        url : Url to be fetched of a page.
+        expected_element: Wait for until this element been lo.
+        time_out: Number of seconds before timing out.
+    """
+    
     url = request.form['url']
+    expected_element = request.form.get('expected_element', 'body')
+    timeout = request.form.get('timeout', 20)
+    timeout = float(timeout)
+    if not url:
+        return
+
+    random_agent = faker.user_agent()
+    # Todo(judy): Generate random user agent.
+
     app.logger.info(">> Start to fetching %s" % url)
     driver.get(url)
 
-    # try:
-    #     WebDriverWait(driver, 10).until(EC.presence_of_element_located(driver.find_element_by_tag_name('body')))
-    #     print "Page is ready!"
-    # except TimeoutException:
-    #     print "Loading took too much time!"
+    try:
+        WebDriverWait(driver, timeout).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, expected_element)))
+        app.logger.info(">> Page is ready!")
+    except TimeoutException:
+        app.logger.info(">> Loading took too much time!")
 
     app.logger.info(">> Phantom have fetched the web page.")
     html = driver.page_source.encode('utf-8')
@@ -32,4 +58,4 @@ def fetch():
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', debug=True)
