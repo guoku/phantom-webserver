22a91d56428ba34f745394d3adc628168abd61d9
Judy <judy.programer@gmail.com>
2016-03-09 15:10:18 +0800

    add chrome driver.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ EXPOSE 5000
 RUN mkdir /usr/app
 WORKDIR /usr/app
 ADD requirements.txt ./
-RUN pip install -r requirements.txt --index-url http://pypi.douban.com/simple
+RUN pip install -r requirements.txt --index-url http://pypi.douban.com/simple --trusted-host pypi.douban.com
 ADD . .
 
-CMD gunicorn -b 0.0.0.0:5000 --access-logfile "-" app:app
+CMD gunicorn -b 0.0.0.0:5000 -t 180 --access-logfile "-" app:app
--- app.py
+++ app.py
@@ -3,21 +3,18 @@
 
 from time import sleep
 from faker import Faker
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response, jsonify, g
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import NoSuchElementException
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-driver = webdriver.Remote(
-    command_executor='http://127.0.0.1:4444/wd/hub',
-    desired_capabilities=DesiredCapabilities.CHROME.copy()
-)
-
 faker = Faker()
 
 
@@ -33,30 +30,54 @@ def get_sg_cookie():
     """
     username = request.form['email']
     password = request.form.get('password', 'guoku1@#')
+    driver = g.driver
 
     if not username:
         return 'username is required!'
 
-    driver.get('http://news.sogou.com/')
-    quit_button = driver.find_element_by_css_selector("ul.topmenu li:nth-child(3) a")
-    if quit_button and quit_button.text == u'退出':
+    driver.get('https://account.sogou.com/web/webLogin')
+    try:
+        quit_button = driver.find_element_by_css_selector("li.logout a")
         quit_button.click()
         sleep(5)
-        driver.get('http://news.sogou.com/')
+        driver.get('https://account.sogou.com/web/webLogin')
+    except NoSuchElementException:
+        pass
 
     app.logger.info("Will try to login as %s." % username)
     try:
         sleep(10)
-        driver.find_element_by_id('loginBtn').click()
-        sleep(10)
-        driver.find_element_by_id('username').send_keys(username)
-        driver.find_element_by_id('password').send_keys(password)
-        driver.find_element_by_id('loginAct').click()
+        driver.find_element_by_css_selector('input[name="username"]').clear()
+        driver.find_element_by_css_selector('input[name="username"]').send_keys(
+            username)
+        driver.find_element_by_css_selector('input[name="password"]').clear()
+        driver.find_element_by_css_selector('input[name="password"]').send_keys(
+            password)
+        driver.find_element_by_css_selector(
+            'form#Login button[type=submit]').click()
         app.logger.info("logged as %s.", username)
 
-        sleep(30)
+        sleep(20)
         driver.get('http://weixin.sogou.com/')
-        sleep(5)
+        print 'visited weixin.sogou.com'
+        sleep(10)
+        driver.get('http://weixin.sogou.com/weixin?type=1&query=shenyebagua818')
+        print 'searched by weixin.sogou.com'
+        sleep(10)
+        try:
+            driver.find_element_by_css_selector("div.results div").click()
+            print 'clicked'
+            print driver.window_handles
+            sleep(3)
+            print driver.window_handles
+            w2 = driver.window_handles[1]
+            sleep(3)
+            driver.switch_to.window(w2)
+            print 'switched'
+        except NoSuchElementException as e:
+            print e.message
+            pass
+        sleep(10)
         app.logger.info("getting cookies......")
         cookie = '; '.join(
             '{}={}'.format(c['name'], c['value'])
@@ -76,8 +97,9 @@ def fetch():
         expected_element: Wait for until this element been lo.
         time_out: Number of seconds before timing out.
     """
-    
+
     url = request.form['url']
+    driver = g.driver
     expected_element = request.form.get('expected_element', 'body')
     timeout = request.form.get('timeout', 15)
     timeout = float(timeout)
@@ -89,7 +111,7 @@ def fetch():
 
     try:
         WebDriverWait(driver, timeout).until(EC.presence_of_element_located(
-                (By.CSS_SELECTOR, expected_element)))
+            (By.CSS_SELECTOR, expected_element)))
         app.logger.info(">> Page is ready!")
     except TimeoutException:
         app.logger.info(">> Loading took too much time!")
@@ -97,6 +119,24 @@ def fetch():
     app.logger.info(">> Phantom have fetched the web page.")
     html = driver.page_source.encode('utf-8')
     return Response(html, mimetype='text/xml')
+
+
+@app.before_request
+def before_request():
+    driver = webdriver.Remote(
+        # command_executor='http://10.0.2.49:4444/wd/hub',
+        command_executor='http://192.168.99.100:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME.copy()
+    )
+    g.driver = driver
+
+
+@app.teardown_request
+def teardown_request(exception):
+    print '>>  shut down. %s: ', getattr(g, 'driver', 'none.')
+    if hasattr(g, 'driver'):
+        g.driver.close()
+        g.driver.quit()
 
 
 if __name__ == "__main__":
